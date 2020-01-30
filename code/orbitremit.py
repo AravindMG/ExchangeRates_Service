9bd271bb897acbdaa8f67ae895df64e9a9d10758
AravindMG <aravind@bravenewcoin.com>
2020-01-30 16:05:48 +1300

    written as csv

--- code/orbitremit.py
+++ code/orbitremit.py
@@ -1,20 +1,22 @@
 import requests
+import pandas as pd
 
 orbitRemit_rate_url = "https://secure.orbitremit.com/api/v2/rate.json?send_currency=NZD&payout_currency=INR"
 response = requests.get(orbitRemit_rate_url)
 data = response.json()
+or_list = []
 
 if __name__ == "__main__":
     print(data)
     for item, value in data.items():
         if item == 'data':
-            print("send_currency = " + value['send_currency'])
-            print("payout_currency = " + value['payout_currency'])
-            print("rate = " + value['rate'])
-            print("quote_time = " + value['quote_time'])
-            print("expiry_time = " + value['expiry_time'])
-            print("exchange_rate = " + value['exchange_rate'])
-    # for item, value in data['data'].items():
-    #     print(item + " = ", value)
-    #     if item == 'rate':
-    #         print(value)
+            or_list.append([value['send_currency'], value['payout_currency'], value['rate'], value['quote_time'],
+                           value['expiry_time'], value['exchange_rate']])
+            print(or_list)
+
+    dataset = pd.DataFrame(or_list)
+    dataset.columns = ['send_currency', 'payout_currency', 'rate', 'quote_time', 'expiry_time', 'exchange_rate']
+    dataset.dropna(axis=0, how='any', inplace=True)
+    dataset.index = pd.RangeIndex(len(dataset.index))
+    dataset.sample(frac=0.5, replace=True)
+    export_csv = dataset.to_csv(r"C:\Users\Admin\Documents\My_project\exchange-rate.csv", index=None, header=True)
